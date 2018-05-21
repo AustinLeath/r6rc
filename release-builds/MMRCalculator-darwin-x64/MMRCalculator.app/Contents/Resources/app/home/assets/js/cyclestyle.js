1a0814ac7bfc4c6f97a7d2eee36975a40bdabe94
AustinLeath <austinleath18@gmail.com>
2018-05-21 16:06:23 -0500

    Current version as of 5/21/18

--- release-builds/MMRCalculator-darwin-x64/MMRCalculator.app/Contents/Resources/app/home/assets/js/cyclestyle.js
+++
@@ -1,5 +0,0 @@
-function myFunction() {
- document.getElementById("background").style.backgroundColor = "#0375B4";
- document.getElementById('btn1').disabled=true;
- document.getElementById('btn2').disabled=false;
-};
