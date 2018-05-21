7eb6fdbe61de95e5f94ebd92e4ddacb2da26fbec
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-05-21 14:13:25 -0500

    Current version as of 5/21/18

--- release-builds/MMRCalculator-darwin-x64/MMRCalculator.app/Contents/Resources/app/home/assets/js/normalizestyle.js
+++
@@ -1,5 +0,0 @@
-function myFunction1() {
-  document.getElementById('background').style.backgroundColor = "inherit";
-  document.getElementById('btn2').disabled=true;
-  document.getElementById('btn1').disabled=false;
-};
