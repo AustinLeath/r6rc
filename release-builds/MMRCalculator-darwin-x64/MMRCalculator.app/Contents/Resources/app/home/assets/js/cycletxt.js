4ec03d1832f97e6dca2084f31efaf8f2f161a4de
root <root@raspberrypi>
2018-05-21 14:16:59 -0500

    Current version as of 5/21/18

---
+++ release-builds/MMRCalculator-darwin-x64/MMRCalculator.app/Contents/Resources/app/home/assets/js/cycletxt.js
@@ -0,0 +1,12 @@
+var h1 = $('h1[id^="cycletxt-"]').hide(),
+    i = 0;
+
+(function cycletxt() {
+
+    h1.eq(i).fadeIn(2250)
+        .delay(10000)
+        .fadeOut(2250, cycletxt);
+
+    i = ++i % h1.length;
+
+})();
---
+++ release-builds/MMRCalculator-darwin-x64/MMRCalculator.app/Contents/Resources/app/home/assets/js/normalizestyle.js
@@ -0,0 +1,5 @@
+function myFunction1() {
+  document.getElementById('background').style.backgroundColor = "inherit";
+  document.getElementById('btn2').disabled=true;
+  document.getElementById('btn1').disabled=false;
+};
