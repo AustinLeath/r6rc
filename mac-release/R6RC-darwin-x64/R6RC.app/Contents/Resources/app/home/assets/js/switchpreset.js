3daef70c8a64980bb559d16c16ae51cae303663a
AustinLeath <austinleath18@gmail.com>
2018-08-06 00:09:41 -0500

    Current version as of 8/6/18

--- mac-release/R6RC-darwin-x64/R6RC.app/Contents/Resources/app/home/assets/js/switchpreset.js
+++
@@ -1,6 +0,0 @@
-function preset() {
- document.getElementById("preset").style.display = "block";
- document.getElementById("custom").style.display = "none";
- document.getElementById('presetbtn').disabled=true;
- document.getElementById('custombtn').disabled=false;
-};
