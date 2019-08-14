fee5d834b8782948692ac8bd08a098f6374d6e1e
AustinLeath <austinleath18@gmail.com>
2019-08-14 17:31:46 -0500

    Implemented splash screen, got rid of React

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ const {autoUpdater} = require("electron-updater");
 const name = app.getName();
 const version = app.getVersion();
 
-console.log('LOG: App initialized on platform: ' + process.platform);
+console.log('App initialized on platform: ' + process.platform);
 
 let win;
+let loadwin;
 
 function sendStatusToWindow(text) {
   console.log(text);
@@ -34,9 +35,34 @@ function createDefaultWindow() {
     win = null;
   });
   win.loadURL(`file://${__dirname}/index.html#v${version}`);
+    loadwin.destroy();
   return win;
 }
 
+
+function createLoadWindow() {
+  loadwin = new BrowserWindow
+  ({
+    width: 300,
+    height: 300,
+    frame: false,
+    resizable: false,
+    backgroundColor: '#1c1d26',
+    autoHideMenuBar: true
+  });
+  loadwin.on('closed', () => {
+    loadwinwin = null;
+  });
+  loadwin.loadURL(`file://${__dirname}/loader.html#v${version}`);
+  //create r6rc application window
+
+  setTimeout(function () {
+    createDefaultWindow();
+    console.log("Loaded R6RC from launch page");
+  }, 4000);
+
+  return loadwin;
+}
 /*
 app.setJumpList([
   {
@@ -95,24 +121,24 @@ app.setJumpList([
 
 let template = []
   // Windows Menu
-  console.log('LOG: Menu loaded for ' + name + ' on platform: ' + process.platform);
+  console.log('Menu loaded for ' + name + ' on platform: ' + process.platform);
   template.unshift({
     label: name,
     submenu: [
       {
         label: 'Join the Discord',
         accelerator: 'Shift+Control+D',
-        click() { console.log('LOG: Shift+Control+D has been pressed'); shell.openExternal('https://discord.gg/NaAmbbb'); }
+        click() { console.log('Shift+Control+D has been pressed'); shell.openExternal('https://discord.gg/NaAmbbb'); }
       },
       {
         label: 'Learn More',
         accelerator: 'Control+L',
-        click() { console.log('LOG: Control+L has been pressed'); shell.openExternal('https://www.github.com/austinleath/r6rc'); }
+        click() { console.log('Control+L has been pressed'); shell.openExternal('https://www.github.com/austinleath/r6rc'); }
       },
       {
         label: 'Donate',
         accelerator: 'Control+D',
-        click() { console.log('LOG: Control+D has been pressed'); shell.openExternal('https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3NS3ZERCW9GD8'); }
+        click() { console.log('Control+D has been pressed'); shell.openExternal('https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3NS3ZERCW9GD8'); }
       },
       {
         label: 'Fullscreen',
@@ -122,12 +148,12 @@ let template = []
       {
         label: 'Minimize',
         accelerator: 'Control+M',
-        click() { console.log('LOG: Control+M has been pressed'); win.minimize();}
+        click() { console.log('Control+M has been pressed'); win.minimize();}
       },
       {
         label: 'Quit',
         accelerator: 'Control+Q',
-        click() { console.log('LOG: Control+Q has been pressed'); win.close(); }
+        click() { console.log('Control+Q has been pressed'); win.close(); }
       }
     ]
   })
@@ -135,10 +161,10 @@ let template = []
 function fullScreenModule() {
   if ( win.isFullScreen(true) ) {
       win.setFullScreen(false);
-      console.log('LOG: Application exited fullscreen');
+      console.log('Application exited fullscreen');
   } else {
       win.setFullScreen(true);
-      console.log('LOG: Application entered fullscreen');
+      console.log('Application entered fullscreen');
   }
 }
 
@@ -156,31 +182,31 @@ app.on('second-instance', (event, commandLine, workingDirectory) => {
 
 autoUpdater.on('checking-for-update', () => {
   sendStatusToWindow('Checking for update...');
-  console.log('LOG: Checking for update...');
+  console.log('Checking for update...');
 });
 autoUpdater.on('update-available', (info) => {
   sendStatusToWindow('An update is available! Downloading...');
-  console.log('LOG: An update is available! Downloading...');
+  console.log('An update is available! Downloading...');
 });
 autoUpdater.on('update-not-available', (info) => {
   sendStatusToWindow('All up to date!');
-  console.log('LOG: All up to date!');
+  console.log('All up to date!');
 });
 autoUpdater.on('error', (err) => {
   sendStatusToWindow('There was a problem downloading your update. ' + err);
-  console.log('LOG: There was a problem downloading your update. ' + err);
+  console.log('There was a problem downloading your update. ' + err);
 });
 autoUpdater.on('update-downloaded', (info) => {
   sendStatusToWindow('Update downloaded, restart to install.');
-  console.log('LOG: Update downloaded, restart to install.');
+  console.log('Update downloaded, restart to install.');
 });
 app.on('ready', () => {
   autoUpdater.checkForUpdatesAndNotify();
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
-  createDefaultWindow();
+  createLoadWindow();
 });
 app.on('window-all-closed', () => {
   app.quit();
-  console.log('LOG: Application has been closed successfully');
+  console.log('Application has been closed successfully');
 });
